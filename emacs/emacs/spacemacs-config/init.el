bd7bea78801d0eb8155033f14c72a99725910351
Christian Westrom <c.westrom@westrom.xyz>
2022-01-06 09:18:52 -0500

    attempt to get keychain working in emacs

--- emacs/emacs/spacemacs-config/init.el
+++ emacs/emacs/spacemacs-config/init.el
@@ -373,7 +373,7 @@ This function should only modify configuration layer settings."
    ;; `dotspacemacs/user-config'. To use a local version of a package, use the
    ;; `:location' property: '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '()
+   dotspacemacs-additional-packages '(keychain-environment)
    ;; TODO figure out ime on emacs
    ;;dotspacemacs-additional-packages '(imbot)
 
