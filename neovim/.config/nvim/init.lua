e8640083f7f2e180c5eac58672eaa2c1d1f70e34
Christian Westrom <c.westrom@westrom.xyz>
2022-01-12 18:14:12 -0500

    add visual whitespace indicators

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -13,6 +13,14 @@ vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 
+-- Command to see all whitespace
+vim.opt.listchars = {eol = "¬",
+                     tab = ">·",
+                     trail = "~",
+                     extends = ">",
+                     precedes = "<",
+                     space = "."}
+
 -- Line numbers
 vim.o.number = true
 vim.o.relativenumber = true
