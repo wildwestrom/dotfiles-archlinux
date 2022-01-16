46a49ffd14db212f3ac3eb3d5a6f95f7f51c49e2
Christian Westrom <c.westrom@westrom.xyz>
2022-01-16 00:46:03 -0500

    improve the script a bit

--- scripts/.local/bin/chemacs-swtich.fish
+++ scripts/.local/bin/chemacs-swtich.fish
@@ -1,12 +1,16 @@
 #!/usr/bin/env fish
 
-
-set usage "Use this script to change chemacs profiles:
-    chemacs-switch.fish [PROFILE_NAME]"
+set profile_file $XDG_CONFIG_HOME/chemacs/profile
 
 if count $argv > /dev/null
     echo "Set emacs profile to $argv[1]."
     echo $argv[1] > $XDG_CONFIG_HOME/chemacs/profile
 else
-    echo $usage
+    echo "Use this script to change chemacs profiles.
+    chemacs-switch.fish [PROFILE_NAME]
+    "
+    echo "Your profile is currently set to:"
+    bat -p $XDG_CONFIG_HOME/chemacs/profile
+    echo "Here's your current profiles.el file:"
+    bat -p $XDG_CONFIG_HOME/chemacs/profiles.el
 end
