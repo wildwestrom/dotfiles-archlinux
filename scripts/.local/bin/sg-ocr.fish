e1c57c2d54d1cc665adcad966412a3d577541d67
Christian Westrom <c.westrom@westrom.xyz>
2022-03-25 16:21:26 -0400

    add an option to capture vertical text

--- scripts/.local/bin/sg-ocr.fish
+++ scripts/.local/bin/sg-ocr.fish
@@ -1,13 +1,6 @@
 #!/usr/bin/env fish
 
-argparse h/help s/slurp l/loop -- $argv
-
-# If the flag is set, then you can pick an area.
-if set -q _flag_s
-    set area (slurp)
-else
-    set area "0,760 1920x229"
-end
+argparse h/help s/slurp l/loop v/vertical -- $argv
 
 # Display usage/help.
 if set -q _flag_h
@@ -16,7 +9,21 @@ if set -q _flag_h
     echo -e "  -h, --help\tDisplay this help menu."
     echo -e "  -s, --slurp\tCapture a specific area."
     echo -e "  -l, --loop\tLoop this script every 3 seconds."
+    echo -e "  -v, --vertical\tOCR on vertical text instead of horizontal text."
     exit 0
+end
+
+# If the flag is set, then you can pick an area.
+if set -q _flag_s
+    set area (slurp)
+else
+    set area "0,760 1920x229"
+end
+
+if set -q _flag_v
+    set direction "-v"
+else
+    set direction "-h"
 end
 
 # The OCR Function.
@@ -24,7 +31,7 @@ function ocr
     # Capture an area of the screen.
     grim -g $area -t png /tmp/sg-img
     # Use gazou's amazing OCR and copy to Wayland clipboard.
-    gazou -h /tmp/sg-img | tr -d "\n" | wl-copy
+    gazou $direction /tmp/sg-img | tr -d "\n" | wl-copy
 end
 
 ### Main Entry Point ###
