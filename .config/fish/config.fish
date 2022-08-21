1bbaff22a104b8326b20587247d4cbffa71be1e5
Christian Westrom <c.westrom@westrom.xyz>
2022-08-21 05:11:23 -0400

    clippy alias with all the warnings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -81,6 +81,7 @@ set -gx CARGO_INCREMENTAL 1
 set -gx CARGO_HOME "$XDG_DATA_HOME/cargo"
 set -gx RUSTUP_HOME "$XDG_DATA_HOME/rustup"
 fish_add_path -a "$CARGO_HOME/bin"
+alias clipme "cargo clippy -- -W clippy::pedantic -W clippy::nursery -W clippy::cargo"
 
 # Go
 set -gx GOPATH "$XDG_DATA_HOME/go"
