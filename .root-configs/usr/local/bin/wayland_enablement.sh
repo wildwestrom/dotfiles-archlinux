cc1c25e477553e1e9bd9ba35034e4cc863de83e0
Christian Westrom <c.westrom@westrom.xyz>
2022-11-16 13:34:19 -0500

    add fcitx5 vars for proper ime support

--- .root-configs/usr/local/bin/wayland_enablement.sh
+++ .root-configs/usr/local/bin/wayland_enablement.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 export MOZ_ENABLE_WAYLAND=1
 export CLUTTER_BACKEND=wayland
-export QT_QPA_PLATFORM=wayland-egl
+export QT_QPA_PLATFORM=wayland
 export ECORE_EVAS_ENGINE=wayland-egl
 export ELM_ENGINE=wayland_egl
 export SDL_VIDEODRIVER=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
 export NO_AT_BRIDGE=1
+
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
