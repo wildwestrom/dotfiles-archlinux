c8d9fe2ae86c8c0427b833b841d660a82ecc4409
Christian Westrom <c.westrom@westrom.xyz>
2022-12-06 15:24:05 -0500

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -81,11 +81,8 @@ set -gx CARP_DIR "$HOME/code/community/Carp"
 
 # Rust
 set -gx CARGO_INCREMENTAL 1
-set -gx CARGO_HOME "$XDG_DATA_HOME/cargo"
-set -gx RUSTUP_HOME "$XDG_DATA_HOME/rustup"
 fish_add_path -a "$CARGO_HOME/bin"
 alias clipme "cargo clippy -- -W clippy::all -W clippy::pedantic -W clippy::nursery -W clippy::cargo"
-alias clipme-restricted "cargo clippy -- -W clippy::all -W clippy::restriction -W clippy::pedantic -W clippy::nursery -W clippy::cargo -A clippy::needless-return"
 
 # Go
 set -gx GOPATH "$XDG_DATA_HOME/go"
