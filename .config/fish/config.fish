9034b23977093e6648dbb40edde393ca5e5bd185
Christian Westrom <c.westrom@westrom.xyz>
2023-01-26 09:43:35 +0900

    add gnvim because neovide stopped working

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,7 +10,9 @@ set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx XDG_STATE_HOME "$HOME/.local/state"
 
 # Editor
-set -gx EDITOR "swayhide neovide --neovim-bin $HOME/.local/bin/lvim --nofork"
+set -gx EDITOR "swayhide gnvim --nvim $HOME/.local/bin/lvim"
+# set -gx EDITOR "swayhide neovide --neovim-bin $HOME/.local/bin/lvim --nofork"
+# set -gx EDITOR "$HOME/.local/bin/lvim"
 # set -gx EDITOR "swayhide emacsclient -c"
 
 # Reader
